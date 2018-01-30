22478444d14c190a4394a0225b8c200bcd41c7a5
ghostqu <ghostqu0@gmail.com>
2018-01-30 16:03:18 -0500

    the answer of edit distance

--- practice/session01/EditDistance.cpp
+++ practice/session01/EditDistance.cpp
@@ -9,3 +9,25 @@ c) Replace a character
 
 I will update the answer next monday.
 */
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+  string s = "ABCAB";
+  string t = "ABCC";
+  int m = s.size();
+  int n = t.size();
+  int dp[m + 1][n + 1];
+  for(int i = 0; i <= m; i++) {
+    for(int j = 0; j <= n; j++) {
+      if(i == 0 || j == 0) {
+	dp[i][j] = i > j? i : j;
+	continue;
+      }
+      if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+      else dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;  
+    }
+  }
+  cout << dp[m][n] << endl;
+  return 0;
+}
