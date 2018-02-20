71eb78d30d3f90e48d1fd23d4945050c2c55eb98
ghostqu <ghostqu0@gmail.com>
2018-02-20 11:02:47 -0500

    new bm algor

--- practice/session01/boyerMoore2.cc
+++ practice/session01/boyerMoore2.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int boyerMoore(const string& s, const string& t) {
   int m = s.size(), n = t.size();
   vector<int> map(128, n);
@@ -10,11 +12,19 @@ int boyerMoore(const string& s, const string& t) {
   }
   int i = n - 1;
   while(i < m) {
-    for(int j = n - 1; j >= 0; j--) {
+    int j = n - 1;
+    for(; j >= 0; j--) {
       if(j == 0 && s[i - j] == t[j]) return i - n + 1;
       if(s[i - j] == t[j]) continue;
-      i += map[s[i - j]];
     }
+    i += map[s[i - j]];
   }
   return -1;
+}
+int main() {
+    string s = "here is a simple example";
+    string t = "simple";
+    int res = boyerMoore(s, t);
+    cout << res << endl;
+    return 0;
 }
