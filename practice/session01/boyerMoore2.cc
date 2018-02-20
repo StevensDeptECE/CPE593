0497d4091ecd7b6de35f9b5cd567b65e18fba63f
ghostqu <ghostqu0@gmail.com>
2018-02-20 11:12:51 -0500

    right

--- practice/session01/boyerMoore2.cc
+++ practice/session01/boyerMoore2.cc
@@ -6,15 +6,14 @@ using namespace std;
 int boyerMoore(const string& s, const string& t) {
   int m = s.size(), n = t.size();
   vector<int> map(128, n);
-  for(int i = n - 1; i >= 0; i--) {
-    if(map[t[i]] == n) 
+  for(int i = 0; i < n; i++) { 
       map[t[i]] = n - 1 - i;
   }
   int i = n - 1;
   while(i < m) {
-    int j = n - 1;
-    for(; j >= 0; j--) {
-      if(j == 0 && s[i - j] == t[j]) return i;
+    int j = i;
+    for(; j >= i - n + 1; j--) {
+      if(j == i - n + 1 && s[j] == t[j - i + n - 1]) return i - n + 1;
       if(s[i - j] == t[j]) continue;
     }
     i += map[s[i - j]];
@@ -22,7 +21,7 @@ int boyerMoore(const string& s, const string& t) {
   return -1;
 }
 int main() {
-    string s = "here is a simple example";
+    string s = "here is a very very simple example";
     string t = "simple";
     int res = boyerMoore(s, t);
     cout << res << endl;
