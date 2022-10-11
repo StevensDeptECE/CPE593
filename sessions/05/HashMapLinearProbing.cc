e4a42cf1df1b03581b5358c0758e633aa9d7e19b
Dov Kruger <Dov.Kruger@stevens.edu>
2022-10-10 20:23:40 -0400

    c++ example linear probing

--- sessions/05/HashMapLinearProbing.cc
+++ sessions/05/HashMapLinearProbing.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <cstring>
 using namespace std;
 
 class HashMapLinearProbing {
@@ -25,6 +26,18 @@ private:
     key = key ^ ((key >> 8) | (key << 24));
     return key % capacity; // return a hashed bin number from 0 to capacity-1
   }
+  void grow() {
+    const Node* old = table; // remember the old table
+    table = new Node[capacity * 2];
+    memset(table, 0, capacity * 2 * sizeof(Node));
+    size = 0;
+    for (uint32_t i = 0; i < capacity; i++)
+      if (old[i].key != 0) {
+        add(old[i].key, old[i].val);
+      }
+    capacity *= 2;
+    delete [] old;
+  }
 public:
   HashMapLinearProbing(uint32_t capacity) : size(0), capacity(capacity), table(new Node[capacity]){
     for (uint32_t i = 0; i < capacity; i++)
@@ -40,15 +53,41 @@ public:
   void add(uint32_t key, uint32_t val) { //O()
     uint32_t bin = hash(key);
     while (table[bin].key != 0) { // while something is there (collision)
+      if (table[bin].key == key) {
+        table[bin].val = val; // replace the existing value in the hashmap
+        return; // and get out!
+      }
       bin++;
       if (bin >= capacity)
         bin = 0;
     }
     // when we get out, what is the invariant? What are we guaranteed?
     //table[bin].key = 0
+    if (size*2 >= capacity)
+      grow();
     table[bin].key = key;
     table[bin].val = val;
+    size++;
   }
+  // return null if not found, or pointer to the corresponding value
+  const uint32_t* get(uint32_t key) const {
+    uint32_t bin = hash(key); //O(1)
+    while (table[bin].key != 0) {
+      if (table[bin].key == key) {
+        return &table[bin].val; // return a pointer to the corresponding value
+      }
+      bin++;
+      if (bin >= capacity)
+        bin = 0;
+    }
+    return nullptr; // not present!
+  }
+
+  // return true if found, if true, set val by reference
+  bool get(uint32_t key, uint32_t& val) {
+    return false; // TODO: implement me!
+  }
+
   void printHistogram() const {
     uint32_t hist[10] = {0};
     // hist[0] = number of bins with 0 collisions
@@ -81,7 +120,21 @@ int main() {
   //Unsupported HashMapLinearProbing m2 = m;
 
   constexpr uint32_t n = 512;
+  #if 0
   for (uint32_t i = 0; i < n; i++)
     m.add(distribution(gen), i);
   m.printHistogram();
+  #endif
+  for (uint32_t i = 0; i < n; i++)
+    m.add(i, i*2);
+  
+  for (uint32_t i = 0; i< 10; i++) {
+    const uint32_t* p = m.get(i);
+    if (p != nullptr) {
+      cout << i << "==>" << *p << '\n';
+    }
+  }
+
+  
+
 }
